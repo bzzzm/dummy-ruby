d4a4d1a4a79b752e2506816fa503dae3f603f8aa
Mihai Mustea <mihai.mustea@gmail.com>
2016-10-09 16:19:19 +0300

    fixed post

--- app.rb
+++ app.rb
@@ -18,9 +18,19 @@ get '/contact' do
 end
 
 post '/callback' do
+    content_type 'application/pdf'
     data = JSON.parse(request.body.read.to_s)
-    puts data
-    erb :ty, :locals => { :data => data }
+    erb_file = 'views/ty.erb'
+
+    html = ERB.new(File.read(erb_file)).result(OpenStruct.new({'data' => data}).instance_eval { binding }) 
+    kit = PDFKit.new(html, :page_size => 'Letter')
+
+    # kit.stylesheets << 'public/bootstrap.css'
+    # return html
+    return kit.to_pdf
+
+    # data = JSON.parse(request.body.read.to_s)
+    # erb :ty, :locals => { :data => data }
 end
 
 get '/callback' do
